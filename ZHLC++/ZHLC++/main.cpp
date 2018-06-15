86896ea9920616190b197b59c1a7c39add016d2b
ONOliangzhihao <liangzhihao@ono.chat>
2018-06-15 16:04:50 +0800

    Testing the first commit.

---
+++ ZHLC++/ZHLC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ZHLC++
+//
+//  Created by liangzhihao on 2018/6/15.
+//  Copyright © 2018年 lzh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
